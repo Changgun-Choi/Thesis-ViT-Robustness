32e0acf0140653cfe8b06956928f8a29df69d84e
ChangGun <changgun.choi7@gmail.com>
2022-05-17 17:42:18 +0200

    Update

---
+++ VisionTransformer/VisionTransformer/VisionTransformer/results/Results_file.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 16:14:40 2022
+
+@author: ChangGun Choi
+"""
+
+#%%
+# https://colab.research.google.com/drive/1muZ4QFgVfwALgqmrfOkp7trAvqDemckO?usp=sharing
+"Test data: 800 dataset"
+
+"FGSM : Resnet"
+#clean accuracy:  68.4 %
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 68.4 %
+ # Linf norm ≤ 0.0003921568627450981: 50.6 %
+ # Linf norm ≤ 0.001176470588235294: 28.6 %
+#  Linf norm ≤ 0.00392156862745098:  4.2 %
+ # Linf norm ≤ 0.01568627450980392:  0.5 %
+ 
+"FGSM:EfficientNet"
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 80.4 %
+ # Linf norm ≤ 0.0003921568627450981: 70.5 %
+  #Linf norm ≤ 0.001176470588235294: 53.9 %
+  #Linf norm ≤ 0.00392156862745098: 33.8 %
+  #Linf norm ≤ 0.01568627450980392: 24.5 %
+  
+"FGSM: ViT  "
+#clean accuracy:  88.6 %
+#robust accuracy for perturbations with
+#  Linf norm ≤ 0     : 88.6 %
+ # Linf norm ≤ 0.0003921568627450981: 78.7 %
+ # Linf norm ≤ 0.001176470588235294: 64.3 %
+ # Linf norm ≤ 0.00392156862745098: 42.0 %
+ # Linf norm ≤ 0.01568627450980392: 26.7 %
+
+"FGSM: DeiT "    
+" Training data-efficient image transformers & distillation through attention"
+#robust accuracy for perturbations with
+#  Linf norm ≤ 0     : 86.6 %
+ # Linf norm ≤ 0.0003921568627450981: 75.6 %
+  #Linf norm ≤ 0.001176470588235294: 65.8 %
+  #Linf norm ≤ 0.00392156862745098: 55.3 %
+  #Linf norm ≤ 0.01568627450980392: 43.5 %
+    
+"FGSM: Swin"   "Robust to FGSM" 
+#clean accuracy:  87.6 %
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 87.6 %
+  #Linf norm ≤ 0.0003921568627450981: 76.6 %
+  #Linf norm ≤ 0.001176470588235294: 74.0 %
+  #Linf norm ≤ 0.00392156862745098: 67.4 %
+  #Linf norm ≤ 0.01568627450980392: 60.9 %
+#%%  
+"PGD: ResNet"
+ # Linf norm ≤ 0     : 68.4 %
+  #Linf norm ≤ 0.0003921568627450981: 51.0 %
+  #Linf norm ≤ 0.001176470588235294: 24.1 %
+  #Linf norm ≤ 0.00392156862745098:  0.4 %
+  #Linf norm ≤ 0.01568627450980392:  0.0 %
+"PGD: EfficientNet"
+#  Linf norm ≤ 0     : 72.2 %
+#  Linf norm ≤ 0.0003921568627450981: 61.3 %     ViT > EfficientNet
+#  Linf norm ≤ 0.001176470588235294: 43.8 %      ViT > EfficientNet
+#  Linf norm ≤ 0.00392156862745098: 12.7 %       EfficientNet > ViT
+#  Linf norm ≤ 0.011764705882352941:  0.6 %      EfficientNet > ViT
+
+"PGD: Vit"  
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 88.6 %
+  #Linf norm ≤ 0.0003921568627450981: 78.1 %    ViT > EfficientNet
+  #Linf norm ≤ 0.001176470588235294: 56.7 %     ViT > EfficientNet
+  #Linf norm ≤ 0.00392156862745098: 12.5 %      EfficientNet > ViT
+  #Linf norm ≤ 0.01568627450980392:  0.0 %      EfficientNet > ViT
+
+"PGD: DeiT"
+##  Linf norm ≤ 0     : 79.8 %
+  #Linf norm ≤ 0.0003921568627450981: 64.9 %
+  #Linf norm ≤ 0.001176470588235294: 37.7 %
+  #Linf norm ≤ 0.00392156862745098:  6.7 %
+  #Linf norm ≤ 0.011764705882352941:  0.2 %
+  
+"PGD: Swin"  "Not robust to PGD attacks"          # 800
+#clean accuracy:  87.6 %
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 87.6 %
+  #Linf norm ≤ 0.0003921568627450981: 60.0 %
+ # Linf norm ≤ 0.001176470588235294: 23.5 %
+  #Linf norm ≤ 0.00392156862745098:  1.1 %
+  #Linf norm ≤ 0.01568627450980392:  0.0 %
+#%%
+"Fool_attack: resnet"
+#Linf norm ≤ 0     : 68.4 %
+#  Linf norm ≤ 0.0003921568627450981: 48.4 %
+ # Linf norm ≤ 0.001176470588235294: 22.2 %
+  #Linf norm ≤ 0.00392156862745098:  0.4 %
+  #Linf norm ≤ 0.01568627450980392:  0.0 %
+
+"Fool_attack: Efficient"
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 80.4 %
+  #Linf norm ≤ 0.0003921568627450981: 69.2 %
+  #Linf norm ≤ 0.001176470588235294: 46.6 %
+  #Linf norm ≤ 0.00392156862745098: 15.3 %
+  #Linf norm ≤ 0.01568627450980392:  1.2 %
+  
+"Fool_attack: ViT"
+#  Linf norm ≤ 0     : 88.6 %
+ # Linf norm ≤ 0.0003921568627450981: 77.7 %
+  #Linf norm ≤ 0.001176470588235294: 59.1 %
+  #Linf norm ≤ 0.00392156862745098: 22.9 %
+  #Linf norm ≤ 0.01568627450980392:  0.4 %
+  
+"Fool_attack: DeiT"
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 86.6 %
+ # Linf norm ≤ 0.0003921568627450981: 64.6 %
+ # Linf norm ≤ 0.001176470588235294: 24.1 %
+#  Linf norm ≤ 0.00392156862745098:  1.0 %
+ # Linf norm ≤ 0.01568627450980392:  0.0 %
+
+"Fool_attack: Swin"
+#Linf norm ≤ 0     : 87.6 %
+ # Linf norm ≤ 0.0003921568627450981: 42.4 %
+  #Linf norm ≤ 0.001176470588235294:  5.3 %
+#  Linf norm ≤ 0.00392156862745098:  0.0 %
+  #Linf norm ≤ 0.01568627450980392:  0.0 %
+
+"ViT > Efficient > DeiT > ResNet > Swin"
+
+
+#%%
+"Test data: 800 test dataset, 5 Models tested "
+# Adversarial attack: maximizing the inner optimization problem
+"FGSM:     Swin > DeiT > ViT > EfficientNet > ResNet"  
+"PGD:      EfficientNet > ViT > DeiT > Swin > ResNet"
+"DeepFool: ViT > Efficient > DeiT > ResNet > Swin"
+# minimal perturbation to fool
+# https://velog.io/@wilko97/%EB%85%BC%EB%AC%B8%EB%A6%AC%EB%B7%B0-DeepFool-a-simple-and-accurate-method-to-fool-deep-neural-networks-CVPR-2016
+
+"1. ViT models are robust on FGSM attacks but not on PGD(stronger attack) compared to EfficientNet"
+#   PGD: Multiple steps with learning rate
+#  -> Multiple steps of Gradient attacks affects Robustness of Transformer?
+#  ex. exclude shifted layer??
+  
+
+#  Shifted windows -> Improve clean accuracy but hurt adversarial robustness
+"possible reason: shifted windows limiting self-attention computation harms Robustness"
+
+"3. Feautre learned by ViT would be more generalizable and robust to perturabtion of FGSM, DeepFool not PGD" 
+
+#Future work: 
+#1. Look at frequency analysis of features -> How does learning low-level features affects robustness? 
+#   -> Literature or library? 
+"2. How model size affects Clean Accuracy and Robustness?  ex. CNNs influences compare them"
+#3. ViT + ResNet(CNNs) : Combining them
+#  How Convolution layer affects ViT robustness? 
+#  or Increasing the Transformer Blocks improve robustness? 
+
+"FGSM:     Swin > DeiT > ViT > EfficientNet > ResNet"  
+"PGD:      EfficientNet > ViT > DeiT > Swin > ResNet"
+"DeepFool: ViT > Efficient > DeiT > ResNet > Swin"
+
+"PGD, DeepFool: Swin(shifted window), DeiT(efficient) is less robust than ViTs, EfficientNet"
+#-> shifted windows which limits self-attention computation could harm Robustness?
+#-> Distillation token could harm Robustness? 
+
+#%%
+"Q1. Parameters of PGD - How do steps, step_size affects robustness?"  # AutoATttack
+# Steps are hyperparameter to experiment but how do we fix (step_size and epsilon) in this case?  
+#Since the step size α is on the same scale as the total perturbation bound ϵ
+# it makes sense to choose α to be some reasonably small fraction of ϵ, 
+# and then choose the number of iterations to be a small multiple of ϵ/α => ex. 4, 8, 10 multiply
+
+"Origianl-PGD: Vit"  
+#robust accuracy for perturbations with
+ # Linf norm ≤ 0     : 88.6 %
+  #Linf norm ≤ 0.0003921568627450981: 78.1 %    ViT > EfficientNet
+  #Linf norm ≤ 0.001176470588235294: 56.7 %     ViT > EfficientNet
+  #Linf norm ≤ 0.00392156862745098: 12.5 %      EfficientNet > ViT
+  #Linf norm ≤ 0.01568627450980392:  0.0 %      EfficientNet > ViT0 %
+1. step_size = eps/4
+robust accuracy for perturbations with
+  Linf norm ≤ 0     : 88.6 %
+  Linf norm ≤ 0.0003921568627450981: 76.2 %
+  Linf norm ≤ 0.001176470588235294: 46.2 %
+  Linf norm ≤ 0.00392156862745098:  3.2 %
+  Linf norm ≤ 0.01568627450980392:  0.0 %
+2. step_size=  eps/8 
+robust accuracy for perturbations with
+  Linf norm ≤ 0     : 88.6 %
+  Linf norm ≤ 0.0003921568627450981: 76.0 %
+  Linf norm ≤ 0.001176470588235294: 46.7 %
+  Linf norm ≤ 0.00392156862745098:  4.0 %
+  Linf norm ≤ 0.01568627450980392:  0.0 %
+3. step_size = eps/12
+robust accuracy for perturbations with
+  Linf norm ≤ 0     : 88.6 %
+  Linf norm ≤ 0.0003921568627450981: 75.9 %
+  Linf norm ≤ 0.001176470588235294: 48.0 %
+  Linf norm ≤ 0.00392156862745098:  4.7 %
+  Linf norm ≤ 0.01568627450980392:  0.0 %
+  
+###############################################################################
+"PGD: EfficientNet"
+#  Linf norm ≤ 0     : 72.2 %
+#  Linf norm ≤ 0.0003921568627450981: 61.3 %     ViT > EfficientNet
+#  Linf norm ≤ 0.001176470588235294: 43.8 %      ViT > EfficientNet
+#  Linf norm ≤ 0.00392156862745098: 12.7 %       EfficientNet > ViT
+#  Linf norm ≤ 0.011764705882352941:  0.6 %      EfficientNet > ViT
+
+1. step_size = eps/4
+robust accuracy for perturbations with
+  Linf norm ≤ 0     : 80.4 %
+  Linf norm ≤ 0.0003921568627450981: 70.2 %
+  Linf norm ≤ 0.001176470588235294: 46.0 %
+  Linf norm ≤ 0.00392156862745098:  9.7 %
+  Linf norm ≤ 0.01568627450980392:  0.0 %
+2. step_size=  eps/8 
+robust accuracy for perturbations with
+  Linf norm ≤ 0     : 80.4 %
+  Linf norm ≤ 0.0003921568627450981: 70.3 %
+  Linf norm ≤ 0.001176470588235294: 46.6 %
+  Linf norm ≤ 0.00392156862745098: 10.8 %
+  Linf norm ≤ 0.01568627450980392:  0.0 %
+3. step_size = eps/12  
+  Linf norm ≤ 0     : 80.4 %
+  Linf norm ≤ 0.0003921568627450981: 70.3 %
+  Linf norm ≤ 0.001176470588235294: 47.1 %
+  Linf norm ≤ 0.00392156862745098: 12.5 %
+  Linf norm ≤ 0.01568627450980392:  0.0 %
+  
+"PGD Insight(3 different step_size): Efficient is more robust than ViT when epsilon is larger than 1/255 "
+# [0.1/255, 0.3/255, 1/255, 4/255] 
+
+# 0.1/255    ViT > EfficientNet
+# 0.3/255    ViT > EfficientNet
+# 1/255      EfficientNet > ViT
+# 4/255      EfficientNet > ViT
+
+#%%
+"Q2. Too small epsilons could affect PGD, ViTs? - step_size will be changed depending on epsilons"
+#Original epsilons = [0, 0.1/255, 0.3/255, 1/255, 4/255]  
+#Try smaller epsilons compared to those?" -> 0.5/255, 0.8/255???
+
+
+"Q3. How to analyze why result of Deepfool?"
+
+"Future work: ViT + ResNet(CNNs) - Robustness "
+# Q1. How Convolution layer affects ViT robustness? 
+# Q2. Increasing the Transformer Blocks improve robustness? "
+
+#=================================================
+"New_Q. Evaluate confidence of Rob"
+#- ex. 0.5- dog, 0.3 - cat   --> CNN is confident 99%  
+#PGD: ex. confident on wrong prediction. 
