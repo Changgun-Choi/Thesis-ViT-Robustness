be0be1f01603f76ac43ba1562a18a1995161385b
ChangGun <changgun.choi7@gmail.com>
2022-08-15 17:20:27 +0200

    Update vit_foolbox_robust.py

--- VisionTransformer/VisionTransformer/VisionTransformer/vit_foolbox_robust.py
+++ VisionTransformer/VisionTransformer/VisionTransformer/vit_foolbox_robust.py
@@ -274,6 +274,11 @@ if __name__ == '__main__':
                 else:
                     robust_accuracy = 1 - success.mean(dim=-1)
                     #obust_acc = robust_accuracy / args.attack_epochs
+                
+                if batch_idx == 0:
+                    robust_acc = robust_accuracy / len(val_loader) 
+                else:
+                    robust_acc += robust_accuracy / len(val_loader) 
                     
                 robust_acc = robust_accuracy /len(val_loader) 
                 print(robust_acc)
